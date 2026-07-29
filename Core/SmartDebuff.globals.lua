16e41d4dcd99d03b65bc7be78ccd9a2d2c44fd23
KaspOu <kaspbouh+gitkraken@gmail.com>
2026-07-29 02:29:56 +0200

    :sparkles: AuraContainers
    AuraContainers API
    - Multiple AuraContainer+Slot (1 by button)
    - Global AuraContainer: SpellGuard + ShowSpellIcon
    - IgnoreDebuff cooldown detection
    
    UnitAura APIs fallback
    - IgnoreDebuff fixed
    
    Regrouped constants

--- Core/SmartDebuff.globals.lua
+++ Core/SmartDebuff.globals.lua
@@ -14,7 +14,8 @@ SMARTDEBUFF_OPTIONS_TITLE = SMARTDEBUFF_VERS_TITLE .. " Options";
 
 SMARTDEBUFF_IS_RETAIL = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE);
 SMARTDEBUFF_MACROTEXT = false;
-SMARTDEBUFF_HASSECRETS = true --secretwrap and issecretvalue(secretwrap(true)) or false
+SMARTDEBUFF_HASSECRETS =  true -- not not C_UnitAuras.AddAuraSoundtrue --secretwrap and issecretvalue(secretwrap(true)) or false
+SMARTDEBUFF_OLDAPI = not AuraContainerSortDirection
 SMARTDEBUFF_KEYFORCLICK = GetCVar("ActionButtonUseKeyDown") == "0" and "AnyUp" or "AnyDown"
 
 BINDING_HEADER_SMARTDEBUFF = SMARTDEBUFF_TITLE;
